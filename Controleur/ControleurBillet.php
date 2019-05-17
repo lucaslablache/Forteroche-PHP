8de70bf93eada0c6fc2075ef6f1d3d2188bea231
lablache lucas <lucas.lablache@gmail.com>
2019-05-17 05:50:04 +0200

    Création et complétion de ControleurBillet.php

--- Controleur/ControleurBillet.php
+++ Controleur/ControleurBillet.php
@@ -4,4 +4,35 @@ require_once 'Modele/Billet.php';
 require_once 'Modele/Commentaire.php';
 require_once 'Vue/Vue.php';
 
+class ControleurBillet
+{
+    private $billet;
+    private $commentaire;
+
+    public function __construct()
+    {
+        $this->billet = new Billet();
+        $this->commentaire = new Commentaire();
+
+    }
+
+    //affichage du billet
+    public function billet($idBillet)
+    {
+        $billet = $this->billet->getBillet($idBillet);
+        $commentaires = $this->commentaire->getCommentaires($idBillet);
+        $vue = new Vue("Billet");
+        $vue ->generer(array('billet' => $billet, 'commentaires' => $commentaires));
+    }
+
+
+    public function commenter($auteur, $contenu, $idBillet)
+    {
+        // ajout du commentaire dans la BDD
+        $this->commentaire->ajouterCommentaire($auteur, $contenu, $idBillet);
+        //actualisation
+        $this->billet($idBillet);
+    }
+}
+
 ?>

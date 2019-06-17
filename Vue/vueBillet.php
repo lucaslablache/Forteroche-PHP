db2fbb23a793eb3495df3d0de9663117c60065d4
lablache lucas <lucas.lablache@gmail.com>
2019-06-17 23:22:58 +0200

    Ajouté un formulaire pour poster un commentaire apres un billet (en construction)

--- Vue/vueBillet.php
+++ Vue/vueBillet.php
@@ -15,7 +15,7 @@
         <ul class="list-unstyled">
             <?php foreach ($commentaires as $commentaire): ?>
             <li class="media py-4">
-                <img class="mr-3 avatar" src="/Contenu/avatar.jpg" alt="Generic placeholder image">
+                <img class="mr-3 avatar" src="Contenu/avatar.jpg" alt="Generic placeholder image">
                 <div class="media-body bg-light">
                     <h5 class="mt-0 mb-1"><?= $commentaire['auteur'] ?></h5>
                     <?= $commentaire['contenu'] ?>
@@ -23,7 +23,36 @@
             </li>
             <?php endforeach; ?>
         </ul>
+        <p>
+            <!--
+            dropdown + 2 champs + submit
+            $post['id'] = $billet['id']
+            bouton submit
+            -->
+
+            <!-- Text input-->
+            <div class="form-group">
+                <label class="col-md-8 control-label" for="pseudo">Votre pseudo</label>
+                <div class="col-md-6">
+                    <input id="pseudo" name="pseudo" type="text" placeholder="Pseudo" class="form-control input-md" required="">
+                </div>
+            </div>
+
+            <!-- Text input a changer (text area)-->
+            <div class="form-group">
+                <label class="col-md-8 control-label" for="Message">Votre Message</label>
+                <div class="col-md-12">
+                    <textarea id="Message" name="Message" type="text" placeholder="Message" class="form-control input-md" required=""></textarea>
+                </div>
+            </div>
+
+            <!-- Button -->
+            <div class="form-group">
+                <label class="col-md-8 control-label" for="confirmation">Envoyer</label>
+                <div class="col-md-8">
+                    <button id="confirmation" name="confirmation" class="btn btn-success" action="commenter">Envoyer</button>
+                    <input type="hidden" id="id" value="<?=$billet['id']?>">
+                </div>
+            </div>
+        </p>
     </div>
-
-
-<!-- Ajouter un formulaire pour commenter le post -->

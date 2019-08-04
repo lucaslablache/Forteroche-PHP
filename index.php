eb50f5cd3148fc185b6bf0cf3b528b21d0b38c6e
lablache lucas <lucas.lablache@gmail.com>
2019-08-04 14:46:52 +0200

    Ajout du viewport

--- index.php
+++ index.php
@@ -7,13 +7,10 @@ $routeur->routerRequete();
 
 //a faire =>
 
-// mettre a jour les controleur pour "is admin"
 // tiny MCE mettre en place
 
 // protection utilisateur
 // pas pour les champs tiny MCE
-
-// viewport
 
 //admin des commentaires en backend !!
 // signaler sur le frontoffice

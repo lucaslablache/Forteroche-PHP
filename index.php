ba3282d4a6d6b05c669d8ff7f1c59f7eda965310
lablache lucas <lucas.lablache@gmail.com>
2019-09-06 15:03:41 +0200

    fin de la refonte graphique

--- index.php
+++ index.php
@@ -10,5 +10,5 @@ $routeur->routerRequete();
 // problemes de mise en page HTML
 // header => media querries sur la hauteur + ajouter un background image
 // nav burger positionrelative + top:XXXpx
-// revoir la modération avec vue billet comme exemple
+
 // htc access a finir pour  les autres vues
